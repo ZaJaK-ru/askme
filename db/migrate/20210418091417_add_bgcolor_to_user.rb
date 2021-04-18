f3bba5b5b21e5f8a83c5be6e664e1d77396bcdc3
zvasib <zvasib@ya.ru>
2021-04-18 17:09:10 +0700

    Avatar webpacker fix

--- db/migrate/20210418091417_add_bgcolor_to_user.rb
+++ db/migrate/20210418091417_add_bgcolor_to_user.rb
@@ -1,5 +1,5 @@
 class AddBgcolorToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :bgcolor, :string
+    add_column :users, :bgcolor, :string, default: '#005a55'
   end
 end
